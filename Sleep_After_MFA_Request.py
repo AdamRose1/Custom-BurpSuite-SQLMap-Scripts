ca09c568f9349cfc0c4041c0d8ca629d02bbeb48
Adam Rose <93153300+AdamRose1@users.noreply.github.com>
2024-10-08 07:58:30 -0400

    Update Sleep_After_MFA_Request.py

--- Sleep_After_MFA_Request.py
+++ Sleep_After_MFA_Request.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 
-'''This is a Burp Suite extension I wrote to solve a certain problem Burp Suite has when performing a scan of 'crawl and audit'. 
-The issue is on sites that use mfa for login but only allow the use of the mfa code number once.  Once the mfa code number is used the site does not allow using that same code number again, and so it causes Burp Suite scans of 'crawl and audit' to error on login requests.  
+'''This is a Burp Suite extension I wrote to give the ability of sleeping (waiting) for a certain amount of time between certain requests that are made while not sleeping in between other requests.  
+This solves a specific problem Burp Suite has when performing a scan of 'crawl and audit' on sites that use mfa for login but the site only allows the mfa code number to be used once.  Once the mfa code number is used, the site does not allow using that same code number again, and so it causes Burp Suite scans of 'crawl and audit' to error on login requests.  
 The mfa code number gets renewed to a different number every 30 seconds.  So this extension adds functionality to Burp Suite that allows waiting 30 seconds after each mfa code number that is submitted.  
 The amount of time can easilly be changed by changing the time.sleep amount to a different amount.  
-This extension is helpful not only for solving this problem, but any type of requests that require a waiting time period before issuing the next request.  
+This extension is helpful not only for solving this Burp Suite issue, but any type of requests that require a waiting time period before issuing the next request. 
+ This can be configured by changing the string 'MFA_CODE' it is searching for to trigger the sleep action to a different string from the response.
 '''
 
 from burp import IBurpExtender, IExtensionStateListener, IHttpListener, IHttpRequestResponse, ISessionHandlingAction
@@ -26,8 +27,8 @@ class BurpExtender(IBurpExtender, IExtensionStateListener, IHttpListener, ISessi
             request = messageInfo.getRequest()
             # Get the request body
             body = self._helpers.bytesToString(request).split("\r\n\r\n", 1)[1]
-            # Check if 'SMS_MFA_CODE' is in the body
-            if 'SMS_MFA_CODE' in body:
+            # Check if 'MFA_CODE' is in the body
+            if 'MFA_CODE' in body:
                 # Introduce delay
                 time.sleep(30)
 
