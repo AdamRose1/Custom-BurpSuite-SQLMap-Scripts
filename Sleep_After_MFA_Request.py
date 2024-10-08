139a29fdf53a7ee4ffbcc734cce37ca7f4b91e76
Adam Rose <93153300+AdamRose1@users.noreply.github.com>
2024-10-08 07:33:10 -0400

    Update Sleep_After_MFA_Request.py

--- Sleep_After_MFA_Request.py
+++ Sleep_After_MFA_Request.py
@@ -1,3 +1,12 @@
+#!/usr/bin/env python3
+
+'''This is a Burp Suite extension I wrote to solve a certain problem Burp Suite has when performing a scan of 'crawl and audit'. 
+The issue is on sites that use mfa for login but only allow the use of the mfa code number once.  Once the mfa code number is used the site does not allow using that same code number again, and so it causes Burp Suite scans of 'crawl and audit' to error on login requests.  
+The mfa code number gets renewed to a different number every 30 seconds.  So this extension adds functionality to Burp Suite that allows waiting 30 seconds after each mfa code number that is submitted.  
+The amount of time can easilly be changed by changing the time.sleep amount to a different amount.  
+This extension is helpful not only for solving this problem, but any type of requests that require a waiting time period before issuing the next request.  
+'''
+
 from burp import IBurpExtender, IExtensionStateListener, IHttpListener, IHttpRequestResponse, ISessionHandlingAction
 import time
 
