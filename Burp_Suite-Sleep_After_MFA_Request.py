61c25b17cde3fd57c19eba4ecf0820212645b687
Adam Rose <93153300+AdamRose1@users.noreply.github.com>
2024-10-08 08:14:09 -0400

    Update Burp_Suite-Sleep_After_MFA_Request.py

--- Burp_Suite-Sleep_After_MFA_Request.py
+++ Burp_Suite-Sleep_After_MFA_Request.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 
 '''This is a Burp Suite extension I wrote to give the ability of sleeping (waiting) for a certain amount of time between certain requests that are made while not sleeping in between other requests.  
-This solves a specific problem Burp Suite has when performing a scan of 'crawl and audit' on sites that use mfa for login but the site only allows the mfa code number to be used once.  Once the mfa code number is used, the site does not allow using that same code number again, and so it causes Burp Suite scans of 'crawl and audit' to error on login requests.  
-The mfa code number gets renewed to a different number every 30 seconds.  So this extension adds functionality to Burp Suite that allows waiting 30 seconds after each mfa code number that is submitted.  
+This solves a specific problem Burp Suite has when performing a scan of 'crawl and audit' on sites that use mfa for login but the site only allows the mfa code number to be used once.  Once the mfa code number is used, the site does not allow using that same code number again.  This causes Burp Suite scans of 'crawl and audit' to error on login requests since Burp Suite issues many login requests within 30 seconds.
+The mfa code number gets renewed to a different number every 30 seconds.  So this extension adds functionality to Burp Suite that allows sleeping 30 seconds after each mfa code number that is submitted while not sleeping for other requests.  
 The amount of time can easilly be changed by changing the time.sleep amount to a different amount.  
 This extension is helpful not only for solving this Burp Suite issue, but any type of requests that require a sleep before issuing the next request, while not sleeping for all other requests. This can be configured to suit other requests by changing the string 'MFA_CODE' (which triggers the sleep action) to a different string that is expected in the response.
 '''
